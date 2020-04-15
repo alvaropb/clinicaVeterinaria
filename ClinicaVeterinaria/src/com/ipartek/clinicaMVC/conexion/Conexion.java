361bcd553d458a1585b9b5b10eea63c94944c506
alvaro <javaee@javaee-VirtualBox>
2020-04-15 20:48:52 +0200

    Implementada clase conexion

---
+++ ClinicaVeterinaria/src/com/ipartek/clinicaMVC/conexion/Conexion.java
@@ -0,0 +1,38 @@
+package com.ipartek.clinicaMVC.conexion;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+
+
+public class Conexion {
+	private static BasicDataSource dataSource=null;
+	private static DataSource getDataSource() {
+		if(dataSource==null) {
+			dataSource=new BasicDataSource();
+			dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+			dataSource.setUrl("jdbc:mysql://localhost:3306/clinica_veterinaria?serverTimezone=UTC");//se añade timezone para que no de error
+			dataSource.setUsername("debian-sys-maint");
+			dataSource.setPassword("o8lAkaNtX91xMUcV");
+			
+			//pool de conexiones
+			dataSource.setInitialSize(20);
+			dataSource.setMaxIdle(15);
+			dataSource.setMaxTotal(20);
+			dataSource.setMaxWaitMillis(5000);
+			//TODO preguntar:¿no es mas logico pasarle una conexion del pool al DAO desde el servlet?, porque 
+			//tal y como está montado, se crea un pool de 20 conexiones cada vez que se hace una operacion CRUD
+			//pero nunca usará mas de una por accion CRUD
+			
+		}
+		return dataSource;
+	}
+	
+	public static Connection getConexion() throws SQLException {
+		return getDataSource().getConnection();
+	}
+
+}
