f00f062e7a69ba6bb48adc009069c2333483a57b
javaee <javaee@javaee-VirtualBox>
2020-04-16 14:51:13 +0200

    Corregido fallo import en conexion

--- ClinicaVeterinaria/src/com/ipartek/clinicaMVC/conexion/Conexion.java
+++ ClinicaVeterinaria/src/com/ipartek/clinicaMVC/conexion/Conexion.java
@@ -3,18 +3,21 @@ package com.ipartek.clinicaMVC.conexion;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.sql.DataSource;
 
-import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+
 
 
 public class Conexion {
 	private static BasicDataSource dataSource=null;
-	private static DataSource getDataSource() {
+	private static BasicDataSource getDataSource() {
 		if(dataSource==null) {
 			dataSource=new BasicDataSource();
-			dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+			
 			dataSource.setUrl("jdbc:mysql://localhost:3306/clinica_veterinaria?serverTimezone=UTC");//se añade timezone para que no de error
+			dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 			dataSource.setUsername("debian-sys-maint");
 			dataSource.setPassword("o8lAkaNtX91xMUcV");
 			
